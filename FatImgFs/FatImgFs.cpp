fc600e7167ce9d45b0ce49354ce5771e95b13044
NAiveD <32201005+ReaNAiveD@users.noreply.github.com>
2019-11-05 13:29:43 +0800

    init

---
+++ FatImgFs/FatImgFs.cpp
@@ -0,0 +1,47 @@
+﻿// FatImgFs.cpp: 定义应用程序的入口点。
+//
+
+#include "FatImgFs.h"
+
+
+int main()
+{
+	while (true) {
+		char commandLine[100];
+		std::cin.getline(commandLine, 100);
+		char command[20];
+		char flags[20];
+		char arg[50];
+
+		char* currentChar = commandLine;
+		char* cmdp = command;
+		char* flagp = flags;
+		char* argp = arg;
+		while (*currentChar != 0 && *currentChar != ' ') {
+			*cmdp = *currentChar;
+			cmdp++;
+			currentChar++;
+		}
+		while (*currentChar != 0) {
+			while (*currentChar == ' ') {
+				currentChar++;
+			}
+			if (*currentChar == '-') {
+				currentChar++;
+				while (*currentChar != 0 && *currentChar != ' ') {
+					*flagp = *currentChar;
+					flagp++;
+					currentChar++;
+				}
+			}
+			else {
+				while (*currentChar != 0 && *currentChar != ' ') {
+					*argp = *currentChar;
+					argp++;
+					currentChar++;
+				}
+			}
+		}
+	}
+	return 0;
+}

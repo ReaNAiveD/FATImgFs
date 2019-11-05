5455eaac81731dea4a38d940544014ed42f937fb
NAiveD <32201005+ReaNAiveD@users.noreply.github.com>
2019-11-06 01:32:42 +0800

    建立了几个结构体

--- FatImgFs/FatImgFs.cpp
+++ FatImgFs/FatImgFs.cpp
@@ -3,9 +3,192 @@
 
 #include "FatImgFs.h"
 
+struct Date
+{
+	int year;
+	int month;
+	int day;
+};
+
+struct Time
+{
+	int hour;
+	int minute;
+	int second;
+};
+
+struct BootRecord {
+	char shortCmd[3];
+	char oem[8];
+	char bytesPerSection[2];
+	char sectionsPerCluster[1];
+	char bootSize[2];
+	char numFats[1];
+	char rootDirMax[2];
+	char sectionsCount[2];
+	char mediaDescriptorType[1];
+	char sectionsPerFat[2];
+	char sectionsPerTrack[2];
+	char numHeads[2];
+	char hiddenSections[4];
+	char sectionsCount32[4];
+	char driverNum[1];
+	char reserved[1];
+	char bootSig[1];
+	char volId[4];
+	char volLab[11];
+	char fsType[8];
+	int bytsPerSec;
+	int secPerClus;
+	int bootSecCount;
+	int fatsCount;
+	int rootfileCount;
+	int totalSections;
+	int fatSecCount;
+};
+
+struct FsInfo {
+	int secSize;
+	int clusSize;
+	int fatFrom;
+	int dirFrom;
+	int dataFrom;
+	int totalSections;
+};
+
+struct EntryByte
+{
+	char name[13];
+	char attr[1];
+	char reserved[1];
+	char creationSecond[1];
+	char createTime[2];
+	char createDate[2];
+	char accessDate[2];
+	char highClus[2];
+	char modiTime[2];
+	char modiDate[2];
+	char lowClus[2];
+	char size[4];
+};
+
+struct Entry {
+	char name[13];
+	bool readOnly;
+	bool hidden;
+	bool system;
+	bool volumeId;
+	bool directory;
+	bool archieve;
+	bool lfn;
+	Time createTime;
+	Date createDate;
+	Date accessDate;
+	Time modiTime;
+	Date modiDate;
+	int clusterNum;
+	int size;
+};
+
+struct LFNEntryByte {
+	char order[1];
+	char first5[10];
+	char attr[1];
+	char checkSum[1];
+	char next6[12];
+	char zero[2];
+	char final2[4];
+};
+
+Time convertTime(char const* time) {
+	Time result;
+	unsigned short timeNum = *(unsigned short*)time;
+	result.hour = (int)(timeNum >> 11);
+	result.minute = (int)((timeNum & 0x7ff) >> 5);
+	result.second = (int)(timeNum & 0x1f) * 2;
+	return result;
+}
+
+Date convertDate(char const* date) {
+	Date result;
+	unsigned short dateNum = *(unsigned short*)date;
+	result.year = (int)(dateNum >> 9);
+	result.month = (int)((dateNum & 0x1ff) >> 5);
+	result.day = (int)(dateNum & 0x1f);
+	return result;
+}
+
+void completeBoot(BootRecord* record) {
+	record->bytsPerSec = (int) * ((short*)record->bytesPerSection);
+	record->secPerClus = (int)record->sectionsPerCluster[0];
+	record->bootSecCount = (int) * ((short*)record->bootSize);
+	record->fatsCount = (int)record->numFats[0];
+	record->rootfileCount = (int) * ((short*)record->rootDirMax); 
+	record->totalSections = (int) * ((short*)record->sectionsCount);
+	record->fatSecCount = (int) * ((short*)record->sectionsPerFat);
+}
+
+bool isLFN(EntryByte const* entry) {
+	char READ_ONLY = 0x01;
+	char HIDDEN = 0x02;
+	char SYSTEM = 0x04;
+	char VOLUMN_ID = 0x08;
+	return *(entry->attr) & (READ_ONLY | HIDDEN | SYSTEM | VOLUMN_ID);
+}
+
+Entry buildEntry(EntryByte const* entry) {
+	Entry result;
+	for (int i = 0; i < 13; i++) {
+		result.name[i] = entry->name[i];
+	}
+	char READ_ONLY = 0x01;
+	char HIDDEN = 0x02;
+	char SYSTEM = 0x04;
+	char VOLUMN_ID = 0x08;
+	char DIRECTORY = 0x10;
+	char ARCHIEVE = 0x20;
+	result.readOnly = *(entry->attr) & READ_ONLY;
+	result.hidden = *(entry->attr) & HIDDEN;
+	result.system = *(entry->attr) & SYSTEM;
+	result.volumeId = *(entry->attr) & VOLUMN_ID;
+	result.directory = *(entry->attr) & DIRECTORY;
+	result.archieve = *(entry->attr) & ARCHIEVE;
+	result.createTime = convertTime(entry->createTime);
+	result.createDate = convertDate(entry->createDate);
+	result.accessDate = convertDate(entry->accessDate);
+	result.modiTime = convertTime(entry->modiTime);
+	result.modiDate = convertDate(entry->modiDate);
+	result.clusterNum = (int) * (short*)(entry->lowClus);
+	result.size = *(int*)(entry->size);
+}
+
+FsInfo computeBaseInfo(BootRecord const* record) {
+	FsInfo result;
+	result.secSize = record->bytsPerSec;
+	result.clusSize = record->secPerClus * result.secSize;
+	result.fatFrom = record->bootSecCount * result.secSize;
+	result.dirFrom = result.fatFrom + record->fatSecCount * result.secSize * record->fatsCount;
+	result.dataFrom = result.dirFrom + record->rootfileCount * 32;
+	return result;
+}
+
+inline int getDateStartAddress(int clusNum, FsInfo info) {
+	return info.dataFrom + (clusNum - 2) * info.clusSize;
+}
 
 int main()
 {
+	std::string filename = "D:\\nice-\\Code\\FatImgFs\\a.img";
+	//std::string filename = "a.img";
+	std::ifstream inputFile(filename, std::ios::binary);
+	BootRecord record;
+	char* bootp = record.shortCmd;
+	for (int i = 0; i < 62; i++) {
+		*bootp = inputFile.get();
+		bootp++;
+	}
+	completeBoot(&record);
+	FsInfo fsInfo = computeBaseInfo(&record);
 	while (true) {
 		char commandLine[100];
 		std::cin.getline(commandLine, 100);
@@ -45,6 +228,9 @@ int main()
 				}
 				else if (*currentChar != 0) {
 					tooManyArgs = true;
+					while (*currentChar != 0 && *currentChar != ' ') {
+						currentChar++;
+					}
 				}
 			}
 		}
